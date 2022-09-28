d0c635030518d474ce0589d87672b5a02ed0c043
Marie-Claire Fayose <nfayose@gmail.com>
2022-09-28 15:36:53 -0500

    Add logic to process user inpt

--- calculator.py
+++ calculator.py
@@ -7,4 +7,21 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 # Replace this with your code
 equation = input("Enter your equation > ") #ex "+ 1 3"
 tokens = equation.split(' ') # ex ['add', '1', '3']
-
+if tokens[0] == 'q':
+    2 + 1
+if tokens[0] == '+':
+    add(tokens[1], tokens[2])
+if tokens[0] == '-':
+    subtract(tokens[1], tokens[2])
+if tokens[0] == '*':
+    multiply(tokens[1], tokens[2])
+if tokens[0] == '/':
+    divide(tokens[1], tokens[2])
+if tokens[0] == 'square':
+    square(tokens[1], tokens[2])
+if tokens[0] == 'cube':
+    cube(tokens[1], tokens[2])
+if tokens[0] == 'pow':
+    power(tokens[1], tokens[2])
+if tokens[0] == 'mod':
+    mod(tokens[1], tokens[2])
