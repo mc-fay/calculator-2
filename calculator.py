505ad500b48badbab12dce883ad0cd76b5651d59
Marie-Claire Fayose <nfayose@gmail.com>
2022-09-28 16:56:23 -0500

    Fix errors run all operands

--- calculator.py
+++ calculator.py
@@ -5,23 +5,50 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
-equation = input("Enter your equation > ") #ex "+ 1 3"
-tokens = equation.split(' ') # ex ['add', '1', '3']
-if tokens[0] == 'q':
-    2 + 1
-if tokens[0] == '+':
-    add(tokens[1], tokens[2])
-if tokens[0] == '-':
-    subtract(tokens[1], tokens[2])
-if tokens[0] == '*':
-    multiply(tokens[1], tokens[2])
-if tokens[0] == '/':
-    divide(tokens[1], tokens[2])
-if tokens[0] == 'square':
-    square(tokens[1], tokens[2])
-if tokens[0] == 'cube':
-    cube(tokens[1], tokens[2])
-if tokens[0] == 'pow':
-    power(tokens[1], tokens[2])
-if tokens[0] == 'mod':
-    mod(tokens[1], tokens[2])
+
+# while tokens[0] != 'q':
+while True:
+    equation = input("Enter your equation > ") #ex "+ 1 3"
+    tokens = equation.split(' ') # ex ['+', '1', '3']
+    print("TOKENS: ", tokens)
+
+    if tokens[0] != 'q':
+        tokens[1] = float(tokens[1])
+
+    if len(tokens) == 3:
+        tokens[2] = float(tokens[2])
+
+    if tokens[0] == 'q':
+        break
+
+    if tokens[0] == '+':
+        result = add(tokens[1], tokens[2])
+        print(result)
+
+    if tokens[0] == '-':
+        result = subtract(tokens[1], tokens[2])
+        print(result)
+
+    if tokens[0] == '*':
+        result = multiply(tokens[1], tokens[2])
+        print(result)
+
+    if tokens[0] == '/':
+        result = divide(tokens[1], tokens[2])
+        print(result)
+
+    if tokens[0] == 'square':
+        result = square(tokens[1])
+        print(result)
+
+    if tokens[0] == 'cube':    
+        result = cube(tokens[1])
+        print(result)
+
+    if tokens[0] == 'pow':
+        result = power(tokens[1], tokens[2])
+        print(result)
+
+    if tokens[0] == 'mod':
+        result = mod(tokens[1], tokens[2])
+        print(result)
